1341ffc58bdadbe88c11e26299bd9f78ff257e84
Tantely Andrianarivola <tantely.and@onja.org>
2020-09-16 09:33:07 +0300

    deleting and editing people

--- script.js
+++ script.js
@@ -11,12 +11,28 @@ const fetchpeople = async () => {
     return data;
 };
 
-// const fetchPeopleObjects = async () => {
-//   const people = await fetchpeople();
-// }
+const fetchPeopleObjects = async () => {
+  const people = await fetchpeople();
+}
 
-function displayPeopleList (people) {
-  const html = people.map(person => {
+let persons = [];
+
+async function initlocalStorage() {
+  //fetching the data at the first time.
+  // const persons = await fetchpeople();
+  localStorage.setItem("persons", JSON.stringify(persons));
+}
+
+async function restoreFromLocalStorage () {
+  const listOfOeople = JSON.parse(localStorage.getItem('persons'));
+  if (listOfOeople.length) {
+    persons.push(...listOfOeople);
+  }
+  container.dispatchEvent(new CustomEvent('listOfPeopleUpdated'));
+};
+
+function displayPeopleList () {
+  const html = persons.map(person => {
     const dateOfBirth = new Date(person.birthday).getTime();
     const dateNow = new Date(Date.now()).getTime();
     const age = new Date(dateNow - dateOfBirth).toLocaleDateString();
@@ -40,68 +56,58 @@ function displayPeopleList (people) {
   container.innerHTML = html.join("");
 }
 
-// async function initlocalStorage() {
-//   let people = await fetchpeople();
-//   localStorage.setItem("people", JSON.stringify(people));
-// }
-
-async function restoreFromLocalStorage () {
-  const storedPeople = JSON.parse(localStorage.getItem('people'));
-  displayPeopleList(storedPeople);
+function editPerson (e) {
+  const editButton = e.target.matches(".edit");
+  if (editButton) {
+    const button = e.target.closest(".edit");
+    const id = button.dataset.id;
+    editPersonPopup(id);
+  }
 }
 
+async function editPersonPopup(id) {
+  const people = await fetchpeople();
+  const personToEdit = people.find(person => person.id === id);
+  return new Promise(async function(resolve) {
+    const formEl = document.createElement('form');
+    formEl.classList.add("form");
+    formEl.insertAdjacentHTML("afterbegin", `
+      <div class="form-group">
+        <label for="lastName">${personToEdit.lastName}</label>
+        <input type="text" class="form-control" id="${personToEdit.id}" name="lastName" value="${personToEdit.lastName}"> 
+      </div>
+      <div class="form-group">
+        <label for="firstName">${personToEdit.firstName}</label>
+        <input type="text" class="form-control" name="firstName" id="${personToEdit.id}" value="${personToEdit.firstName}">
+      </div>
+      <div class="form-group">
+        <label for="birthday">Birthday</label>
+        <input type="text" id="birthday" class="form-control" name="birthday" id="${personToEdit.birthday}" value="${personToEdit.birthday}">
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    `);
+    document.body.appendChild(formEl);
+    formEl.classList.add("open");
+    formEl.addEventListener("submit", e => {
+      e.preventDefault();
+      const form = e.currentTarget;
+      const newPerson = {
+        id: id,
+        lastName: form.lastName.value,
+        firstName : form.firstName.value,
+        birthday : form.birthday.value,
+        picture : personToEdit.picture,
+      }
 
-// function editPerson (e, people) {
-//   const editButton = e.target.matches(".edit");
-//   if (editButton) {
-//     const button = e.target.closest(".edit");
-//     const id = button.dataset.id;
-//     editPersonPopup(id);
-//   }
-// }
-
-// async function editPersonPopup(id) {
-//   const people = await fetchpeople();
-//   const personToEdit = people.find(person => person.id === id);
-//   return new Promise(async function(resolve) {
-//     const formEl = document.createElement('form');
-//     formEl.classList.add("form");
-//     formEl.insertAdjacentHTML("afterbegin", `
-//       <div class="form-group">
-//         <label for="lastName">${personToEdit.lastName}</label>
-//         <input type="text" class="form-control" id="${personToEdit.id}" name="lastName" value="${personToEdit.lastName}"> 
-//       </div>
-//       <div class="form-group">
-//         <label for="firstName">${personToEdit.firstName}</label>
-//         <input type="text" class="form-control" name="firstName" id="${personToEdit.id}" value="${personToEdit.firstName}">
-//       </div>
-//       <div class="form-group">
-//         <label for="birthday">Birthday</label>
-//         <input type="text" id="birthday" class="form-control" name="birthday" id="${personToEdit.birthday}" value="${personToEdit.birthday}">
-//       </div>
-//       <button type="submit" class="btn btn-primary">Submit</button>
-//     `);
-//     document.body.appendChild(formEl);
-//     formEl.classList.add("open");
-//     formEl.addEventListener("submit", e => {
-//       e.preventDefault();
-//       const form = e.currentTarget;
-//       const newPerson = {
-//         id: id,
-//         lastName: form.lastName.value,
-//         firstName : form.firstName.value,
-//         birthday : form.birthday.value,
-//         picture : personToEdit.picture,
-//       }
-//       const editedPerson = people.find(person => person.id === newPerson.id);
-//       editedPerson.firstName = newPerson.firstName;
-//       editedPerson.lastName = newPerson.lastName;
-//       editedPerson.birthday = newPerson.birthday;
-//       displayPeopleList (people)
-//       formEl.classList.remove("open");
-//     }, {once: true});
-//   })
-// }
+      const editedPerson = persons.find(person => person.id === newPerson.id);
+      editedPerson.firstName = newPerson.firstName;
+      editedPerson.lastName = newPerson.lastName;
+      editedPerson.birthday = newPerson.birthday;
+      container.dispatchEvent(new CustomEvent('listOfPeopleUpdated'));
+      formEl.classList.remove("open");
+    }, {once: true});
+  })
+}
 
 function deletePerson (e) {
   const deleteBtn = e.target.matches(".delete");
@@ -109,7 +115,7 @@ function deletePerson (e) {
     const idOfPeopleToDelete = e.target.closest('.delete').dataset.id;
     deletePersonPupup(idOfPeopleToDelete);
   }
-}
+};
 
 
 async function deletePersonPupup (idOfPeopleToDelete) {
@@ -127,24 +133,24 @@ async function deletePersonPupup (idOfPeopleToDelete) {
   btnPopup.classList.add("open");
 
   function deletePopup () {
-    btnPopup.classList.remove("open")
-  };
+    btnPopup.classList.remove("open");
+  }
 
   noBtn.addEventListener("click", e => {
     deletePopup()
   });
 
   yesBtn.addEventListener("click", e => {
-    const hi = people.filter(person => person.id !== idOfPeopleToDelete);
-    people = hi;
+    const peopleRest = persons.filter(person => person.id !== idOfPeopleToDelete);
+    persons = peopleRest;
+    container.dispatchEvent(new CustomEvent('listOfPeopleUpdated'));
     deletePopup();
-    displayPeopleList(people)
-    console.log(people);
+    console.log(persons);
   }, {once: true})
 };
 
-// container.addEventListener("click", editPerson);
+container.addEventListener("click", editPerson);
 container.addEventListener("click", deletePerson);
-// fetchPeopleObjects();
-// initlocalStorage();
-restoreFromLocalStorage()
+container.addEventListener("listOfPeopleUpdated", displayPeopleList);
+container.addEventListener("listOfPeopleUpdated", initlocalStorage)
+restoreFromLocalStorage();
