efb2d7d612e76d0ce5784fcfd29c2612080ac6ea
Tantely Andrianarivola <tantely.and@onja.org>
2020-09-18 16:27:55 +0300

    updated fixing dates

--- script.js
+++ script.js
@@ -35,7 +35,6 @@ async function restoreFromLocalStorage() {
   if (!persons) {
     fetchpeople();
   }
-  displayPeopleList(persons);
   container.dispatchEvent(new CustomEvent('listOfPeopleUpdated'));
 };
 
@@ -123,11 +122,7 @@ function submitForm (e) {
   e.preventDefault();
   const form = e.currentTarget;
   const birthDate = form.birthday.value;
-  const dateArr = birthDate.split("-");
-  //format date to mm-dd-yy
-  const date = `${dateArr[1]}/${dateArr[2]}/${dateArr[0]}`;
-  const dateTime = new Date(`${date}`).getTime();
-  console.log(date);
+  const dateTime = Date.parse(birthDate);
   console.log(dateTime);
   //create an obj for the new pers
   const newPerson = {
@@ -231,9 +226,12 @@ async function deletePersonPupup (idOfPeopleToDelete) {
   const btnPopup = document.createElement('div');
   btnPopup.classList.add('div');
   btnPopup.textContent = `Are you sure you want to delete ${peopleToDelete.firstName} ${peopleToDelete.lastName}`
+  //add both "yes" and "no" button to the container
   btnPopup.appendChild(yesBtn);
   btnPopup.appendChild(noBtn);
+  //add the container to the DOM
   document.body.appendChild(btnPopup);
+  //open the popup by adding classlist of "open"
   btnPopup.classList.add("open");
 
   //function to delete popup 
