3b7fc7d4acfe2287fda11349aea4b5370ac702ef
Tantely Andrianarivola <tantely.and@onja.org>
2020-09-28 15:18:22 +0300

    deleted some irrelevant stuff

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-// "use strict";
 // import { format, compareAsc } from 'date-fns';
 //I need to fetch but how : create a function to fetch the data from person.json
 //put the data in the local storage
 /* *********************************** */
 
-// let persons = [];
-
-import persons, { container, addBtn, formEl, initlocalStorage, restoreFromLocalStorage } from "./variables.js";
+import { container, addBtn, formEl, initlocalStorage, restoreFromLocalStorage } from "./variables.js";
 import { deletePerson } from "./utility/delete.js";
 import { editPerson } from "./utility/edit.js";
 import { submitForm, showForm } from "./utility/addPeople.js";
